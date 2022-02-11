2df6607c56468f10c7868b95863859804834737f
Gabor Nagy <gaborka19890603@gmail.com>
2022-02-11 22:05:26 +0100

    Add Tron, and chain IDs

---
+++ src/entities/wtrx.ts
@@ -0,0 +1,8 @@
+import { Token } from "."
+import { ChainId } from ".."
+
+export const WTRX: { [chainId: number]: Token} = {
+  [ChainId.MAINNET]: new Token(ChainId.MAINNET, '0x891CDB91D149F23B1A45D9C5CA78A88D0CB44C18', 6, 'WTRX', 'Wrapped TRX'),
+  [ChainId.NILE]: new Token(ChainId.NILE, '0x8f44113A985076431b77f6078f0929f949cB8836', 6, 'WTRX', 'Wrapped TRX'),
+  [ChainId.SHASTA]: new Token(ChainId.SHASTA, '0x891CDB91D149F23B1A45D9C5CA78A88D0CB44C18', 6, 'WTRX', 'Wrapped TRX'),
+}
