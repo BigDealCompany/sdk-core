d5f6bbfe924f97902b921584b8076c3b6e79ac38
Gabor Nagy <gaborka19890603@gmail.com>
2022-02-12 13:21:40 +0100

    Export Tron and wtrx

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -2,6 +2,8 @@ export * from './fractions'
 
 export * from './currency'
 export * from './ether'
+export * from './tron'
 export * from './nativeCurrency'
 export * from './token'
 export * from './weth9'
+export * from './wtrx'
